66cb7eb427acacccfdbd5c80a88c1506c5d33bd8
VIP-PC02\vip <sanyi0127@sina.com>
2018-04-26 10:31:26 +0800

    add datasync project.

---
+++ dotnetcore/datasync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace datasync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
